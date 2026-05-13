9306ad0a54e33ea4274f37c22fa24f41d7f9db41
Nicoaz25 <nicoadza2@gmail.com>
2026-05-13 16:43:53 +0200

    Si

---
+++ vlc/src/main/java/com/todo/vlc/model/Tarea.java
@@ -0,0 +1,69 @@
+package com.todo.vlc.model;
+
+public class Tarea {
+
+    private int id_tarea;
+    private String titulo;
+    private String descripcion;
+    private int prioridad;
+    private String fecha_entrega;
+    private String estado;
+
+    public Tarea(int id_tarea, String titulo, String descripcion, int prioridad, String fecha_entrega, String estado) {
+        this.id_tarea = id_tarea;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+        this.fecha_entrega = fecha_entrega;
+        this.estado = estado;
+    }
+
+    public int getId_tarea() {
+        return id_tarea;
+    }
+
+    public void setId_tarea(int id_tarea) {
+        this.id_tarea = id_tarea;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public String getFecha_entrega() {
+        return fecha_entrega;
+    }
+
+    public void setFecha_entrega(String fecha_entrega) {
+        this.fecha_entrega = fecha_entrega;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
