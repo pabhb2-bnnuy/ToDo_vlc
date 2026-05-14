fbbf3c99c5019f36dce25aaf38be9863b38f2db9
Nicoaz25 <nicoadza2@gmail.com>
2026-05-14 18:26:13 +0200

    Datos gurdados correctamente

--- vlc/src/main/java/com/todo/vlc/Controller/postMapping.java
+++ vlc/src/main/java/com/todo/vlc/Controller/postMapping.java
@@ -6,12 +6,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class postMapping {
 
-    @PostMapping("/register")
+    @Autowired
+    private DataSource dataSource;
+
+    @PostMapping("/inicioSesion")
     public String iniciarSesion(
 
             @RequestParam("mail") String email,
@@ -19,9 +29,46 @@ public class postMapping {
 
     ) {
         Usuario usuario = new Usuario(email, password);
-        
-        
 
-        return "index";
+        try(Connection con = dataSource.getConnection();){
+            
+            String sql = "SELECT * FROM usuarios WHERE email = ? AND passwrd = ?";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            String emaill = null;
+            String contraseña = null;
+
+            System.out.println(email);
+            System.out.println(password);
+
+            if (rs.next()) {
+                emaill = rs.getString("email");
+                contraseña = rs.getString("passwrd");
+            }
+            System.out.println(emaill);
+            System.out.println(contraseña);
+
+            if (emaill.equals(email)){
+                if(contraseña.equals(password)){
+                    return "index";
+                }else{
+
+                }
+            }else{
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "inicioSesion";
     }
+
 }
