8c9312605512cf0269df976969fec48428874384
Nicoaz25 <nicoadza2@gmail.com>
2026-05-14 16:42:54 +0200

    Inicio de sesion terminado

---
+++ vlc/src/main/java/com/todo/vlc/Controller/postMapping.java
@@ -0,0 +1,28 @@
+package com.todo.vlc.Controller;
+
+import com.todo.vlc.model.Usuario;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Controller
+public class postMapping {
+
+    @PostMapping("/register")
+    public String iniciarSesion(
+
+            @RequestParam("mail") String email,
+            @RequestParam("password") String password
+
+    ) {
+
+        
+        
+        return "menu";
+    }
+}
