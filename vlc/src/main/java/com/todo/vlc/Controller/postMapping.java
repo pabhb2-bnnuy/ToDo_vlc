881c80d7d3834740059bd44a32b401a28aa5210d
Nicoaz25 <nicoadza2@gmail.com>
2026-05-14 17:44:15 +0200

    Datos de ingreso inicio de sesion

--- vlc/src/main/java/com/todo/vlc/Controller/postMapping.java
+++ vlc/src/main/java/com/todo/vlc/Controller/postMapping.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
-
 
 @Controller
 public class postMapping {
@@ -20,9 +18,10 @@ public class postMapping {
             @RequestParam("password") String password
 
     ) {
+        Usuario usuario = new Usuario(email, password);
+        
+        
 
-        
-        
-        return "menu";
+        return "index";
     }
 }
