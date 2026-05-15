6523babd2f1bdd2d035622859a2b92b35a25b8e7
Nicoaz25 <nicoadza2@gmail.com>
2026-05-15 17:45:06 +0200

    Matches del inicio de sesion

--- vlc/src/main/java/com/todo/vlc/Controller/LoginController.java
+++ vlc/src/main/java/com/todo/vlc/Controller/LoginController.java
@@ -2,28 +2,27 @@ package com.todo.vlc.Controller;
 
 import com.todo.vlc.Repository.UsuarioRepository;
 import com.todo.vlc.model.Usuario;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.net.PasswordAuthentication;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.Optional;
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Controller
-public class postMapping {
-
-    @Autowired
-    private DataSource dataSource;
+public class LoginController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/inicioSesion")
     public String iniciarSesion(
@@ -34,12 +33,9 @@ public class postMapping {
 
     ) {
 
-        try (Connection con = dataSource.getConnection()) {
+        try {
 
             Optional<Usuario> optUsuario = usuarioRepository.findByEmail(email);
-            
-
-            
 
             if (optUsuario == null) {
                 redirectAttributes.addFlashAttribute(
@@ -49,16 +45,24 @@ public class postMapping {
             }
 
             Usuario usuario = optUsuario.get();
-            
+
             System.out.println(usuario.getEmail());
             System.out.println(usuario.getPasswrd());
 
-            if (!usuario.getPasswrd().equals(password)) {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+            if (!encoder.matches(password, usuario.getPasswrd())) {
+
                 redirectAttributes.addFlashAttribute(
                         "error",
                         "Email o contraseña incorrectos");
+
                 return "redirect:/inicioSesion";
             }
+
+            System.out.println("INPUT: " + password);
+            System.out.println("HASH BD: " + usuario.getPasswrd());
+            System.out.println("MATCH: " + encoder.matches(password, usuario.getPasswrd()));
 
         } catch (Exception e) {
             e.printStackTrace();
