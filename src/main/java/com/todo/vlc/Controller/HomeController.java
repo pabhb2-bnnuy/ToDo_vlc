1562153f5f57a074697075b4f486bb6d70a71397
Nicoaz25 <nicoadza2@gmail.com>
2026-05-25 15:40:40 +0200

    Perfil hecho y cambio de contraseña

--- src/main/java/com/todo/vlc/Controller/HomeController.java
+++ src/main/java/com/todo/vlc/Controller/HomeController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import com.todo.vlc.Repository.UsuarioProyectoRepository;
+import com.todo.vlc.Repository.UsuarioRepository;
 import com.todo.vlc.model.Proyecto;
 import com.todo.vlc.model.Usuario;
 import com.todo.vlc.model.UsuarioProyecto;
@@ -17,8 +21,6 @@ public class HomeController {
 
     @Autowired
     private UsuarioProyectoRepository usuarioProyectoRepository;
-
-
 
     @GetMapping("/registrarse")
     public String registro(Authentication authentication) {
@@ -68,6 +70,58 @@ public class HomeController {
         }
 
         return "index";
+    }
+
+    @GetMapping("/perfil")
+    public String perfil(Model model, Authentication authentication) {
+
+        Usuario usuario = (Usuario) authentication.getPrincipal();
+
+        model.addAttribute("usuario", usuario);
+
+        return "perfil";
+    }
+
+    @GetMapping("/cambiar-password")
+    public String cambiarPassword() {
+        return "cambiarPassword";
+    }
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @PostMapping("/cambiar-password")
+    public String actualizarPassword(
+            Authentication authentication,
+            @RequestParam("actual") String actual,
+            @RequestParam("nueva") String nueva,
+            @RequestParam("repetir") String repetir,
+            Model model) {
+
+        Usuario usuario = (Usuario) authentication.getPrincipal();
+
+        // 1. comprobar contraseña actual
+        if (!passwordEncoder.matches(actual, usuario.getPasswrd())) {
+            model.addAttribute("error", "La contraseña actual no es correcta");
+            return "cambiarPassword";
+        }
+
+        // 2. comprobar repetición
+        if (!nueva.equals(repetir)) {
+            model.addAttribute("error", "Las contraseñas no coinciden");
+            return "cambiarPassword";
+        }
+
+        // 3. guardar nueva encriptada
+        usuario.setPasswrd(passwordEncoder.encode(nueva));
+        usuarioRepository.save(usuario);
+
+        model.addAttribute("success", "Contraseña actualizada correctamente");
+
+        return "cambiarPassword";
     }
 
 }
